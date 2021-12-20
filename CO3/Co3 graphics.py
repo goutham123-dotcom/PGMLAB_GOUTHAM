fb1ab54b7cb3c2fb48e4f56c9482a259f85a0522
goutham123-dotcom <55642670+goutham123-dotcom@users.noreply.github.com>
2021-12-20 19:25:41 +0530

    Add files via upload

---
+++ CO3/Co3 graphics.py
@@ -0,0 +1,5 @@
+
+from graphics import rectangle
+from graphics import circle
+rectangle.arear(int(input("enter length of rectangle")),int(input("enter breadth of rectangle")))
+circle.areac(int(input("enter radius of circle")))
